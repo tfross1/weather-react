d1d5afa345a416f86fc8ceac730bdecd07bd06e4
tfross1 <theresaefross@gmail.com>
2023-04-21 22:15:08 -0500

    Commented out showWeather

--- src/Weather.js
+++ src/Weather.js
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 import "./styles.css";
 
 export default function Weather() {
   const [city, setCity] = useState("");
-  const [loaded, setLoaded] = useState(false);
-  const [weather, setWeather] = useState({});
+  const [loaded, ] = useState(false);
+  const [weather, ] = useState({});
 
-  function showWeather(response) {
-    setLoaded(true);
-    setWeather({
-      temperature: response.data.main.temp,
-      wind: response.data.wind.speed,
-      humidity: response.data.main.humidity,
-      description: response.data.weather[0].description,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    });
-  }
+  // function showWeather(response) {
+  //   setLoaded(true);
+  //   setWeather({
+  //     temperature: response.data.main.temp,
+  //     wind: response.data.wind.speed,
+  //     humidity: response.data.main.humidity,
+  //     description: response.data.weather[0].description,
+  //     icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  //   });
+  // }
 
   //function handleSubmit(event) {
     //event.preventDefault();
